46561857b377e0785b8a99c83734c04a9f38f25d
Gabriel Cavazos <a.gabriel.cavazos@gmail.com>
2022-09-10 10:34:09 -0500

    Se descomento anio y numero de serie

---
+++ ejemplo.cpp
@@ -0,0 +1,60 @@
+//simulador pagina de videojuegos 
+
+
+
+#include<iostream> 
+
+#include<string> 
+
+
+
+using namespace std;
+
+struct Articulo {
+    char nombre[50];
+    int numero_serie;
+    int anio;
+};
+
+int main()
+
+{
+    Articulo juegos[4];
+
+    cout << "TiEnDa De ViDeOjUeGoS";
+
+
+
+    cout << "\n1. agregar articulo: ";
+
+    cout << "\n2. modificar articulo: ";
+
+    cout << "\n3. eliminar/articulo: ";
+
+    cout << "\n4. lista de articulos vigentes (opcion a genero, clasificacion: ";
+
+    cout << "\n5. limpiar pantalla: ";
+
+    cout << "\n6. salir. "<<endl;
+
+    int opcion = 0;
+
+    cin >> opcion;
+
+    switch (opcion)
+    {
+    case 1:
+
+        cout << "\n1. agregar el nombre del juego: ";
+
+        cin >> juegos[0].nombre;
+        break;
+    default:
+        break;
+    }
+
+
+
+
+
+}
